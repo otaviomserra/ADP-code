1b4c45d3bec6b2f99ddffc92a4750c03d3fc0338
chicodb <142610159+chicodb@users.noreply.github.com>
2023-09-04 14:48:54 +0200

    vamo

--- event/organisation_event_processor.py
+++ event/organisation_event_processor.py
@@ -16,8 +16,8 @@ env = environ.FileAwareEnv()
 
 class OrganisationEventProcessor(EventProcessor):
 
-    def __init__(self, queue: Queue, kafka_config, token_refresher: IlsApiTokenRefresher, *args, **kwargs):
-        super(OrganisationEventProcessor, self).__init__(queue, kafka_config, *args, **kwargs)
+    def _init_(self, queue: Queue, kafka_config, token_refresher: IlsApiTokenRefresher, *args, **kwargs):
+        super(OrganisationEventProcessor, self)._init_(queue, kafka_config, *args, **kwargs)
         self._token_refresher = token_refresher
 
     def _handle_message(self, message):
@@ -51,30 +51,30 @@ class OrganisationEventProcessor(EventProcessor):
         logger = self._logger
 
         # connection between lanes and next control cycle
-        lanes = ["S001.M003.02.01",  #Linie FB1 im Supermarkt nach dem Testen
-                 "S001.M003.02.02",  #Linie FB2 im Supermarkt nach dem Testen
-                 "S001.M005.01.03",  #Linie FK im Supermarkt nach dem Testen
+        lanes = ["S001.M003.02.01",
+                 "S001.M003.02.02",
+                 "S001.M005.01.03",
                  "S001.M007.01.01",
                  "S001.M007.01.02",
-                 "S001.M007.01.03", #Linie LD2 im Supermarkt des Central Warehouse
+                 "S001.M007.01.03",
                  "S001.M004.01.02",
-                 "S001.M007.01.04", #Linie LK2 im Supermarkt des Central Warehouse
-                 "S001.M007.02.01", #Linie LG1 im Supermarkt des Central Warehouse
-                 "S001.M007.02.02", #Linie LG2 im Supermarkt des Central Warehouse
-                 "S001.M007.02.03", #Linie LG3 im Supermarkt des Central Warehouse
-                 "S001.M007.02.04", #Linie LG4 im Supermarkt des Central Warehouse
-                 "S001.M004.01.01", #Linie LD2 im Supermarkt Lieferant
+                 "S001.M007.01.04",
+                 "S001.M007.02.01",
+                 "S001.M007.02.02",
+                 "S001.M007.02.03",
+                 "S001.M007.02.04",
+                 "S001.M004.01.01",
                  "S001.M004.02.01",
                  "S001.M004.02.02",
                  "S001.M004.03.01",
                  "S001.M004.03.02",
                  "S001.M004.04.01",
                  "S001.M004.04.02",
-                 "S001.M005.03.03"] #Linie FB2 im Supermarkt Fertigung
+                 "S001.M005.03.03"]
         pick_lanes = ["S001.M003.02.01",
                       "S001.M003.02.02",
                       "S001.M005.01.03",
-                      "S001.M007.01.01", #Linie LD1 im Supermarkt des Central Warehouse
+                      "S001.M007.01.01",
                       "S001.M007.01.02",
                       "S001.M007.01.03",
                       "S001.M004.01.02",
@@ -97,13 +97,13 @@ class OrganisationEventProcessor(EventProcessor):
                     "aadf95aa-c11e-41a5-adf3-377b540000ab",
                     "c63c5b2c-b368-4f15-92ff-812733582ba8",
                     "e16d6f63-ffd6-4a1c-83b6-e1ce18dc17a8",
-                    "ffc280cd-13a8-49e9-a06a-1281e826645d", #
+                    "ffc280cd-13a8-49e9-a06a-1281e826645d",
                     "8d20ae37-d762-4d4a-935a-2d07fec2af7e",
-                    "417aec52-81db-459b-9259-21699851a9d1", #Linie LK2 im Supermarkt des Central Warehouse
+                    "417aec52-81db-459b-9259-21699851a9d1",
                     "72638c2d-39fe-426f-aa6c-ba155236f1d0",
-                    "266b32ef-d2e6-4b6a-979d-ab161cd5b83a", #Linie LG2 im Supermarkt des Central Warehouse
-                    "04e45f88-44aa-41b1-b5d9-8183bf3fa935", #Linie LG3 im Supermarkt des Central Warehouse
-                    "bd4e17b4-44a3-40f5-945a-ccf568820eae", #Linie LG4 im Supermarkt des Central Warehouse
+                    "266b32ef-d2e6-4b6a-979d-ab161cd5b83a",
+                    "04e45f88-44aa-41b1-b5d9-8183bf3fa935",
+                    "bd4e17b4-44a3-40f5-945a-ccf568820eae",
                     "7a5dc1a2-d105-40ec-bcda-5f7c7cb2261b",
                     "2f391f1c-0488-4b21-a215-fe77978d3886",
                     "5a899a44-ea75-4358-a20b-0b338212c1fa",
@@ -113,27 +113,26 @@ class OrganisationEventProcessor(EventProcessor):
                     "1a6acfef-0beb-4115-80c2-9c9cf02c6208",
                     "396e2360-6fec-4e18-9072-8b4e2c6c90fe"]
 
-        control_cycle_ids = ["51c97b7a-e490-4430-87e7-4d9a78046b44", #Kontrollzyklus FB1-SF im Supermarkt Fertigung
-                             "15c5e6f6-a406-4f02-bac3-0521d51f86fc", #FB2-SF
-                             "54c4b38d-174a-409f-9d86-5a98bcb464ee", #FB1-SM
-                             "538a71e2-99b9-4e5d-8fab-bdcde4868f0d", #LD1-SL
-                             "e1dc3046-93f5-48ed-b2cf-bd605e3eaff7", #LK1-SL
-                             "50cb3ac3-e7b4-4db1-9690-9c89ff62ef97", #LD2-SL
-                             "05925966-7932-4e7f-91ee-034b21e2ef5b", #LD1-SM
+        control_cycle_ids = ["51c97b7a-e490-4430-87e7-4d9a78046b44",
+                             "15c5e6f6-a406-4f02-bac3-0521d51f86fc",
+                             "54c4b38d-174a-409f-9d86-5a98bcb464ee",
+                             "538a71e2-99b9-4e5d-8fab-bdcde4868f0d",
+                             "e1dc3046-93f5-48ed-b2cf-bd605e3eaff7",
+                             "50cb3ac3-e7b4-4db1-9690-9c89ff62ef97",
+                             "05925966-7932-4e7f-91ee-034b21e2ef5b",
                              "5624ba3f-327e-4bd7-b021-81a9ac08f6cc",
-                             "f1749739-f4ac-4206-af28-5bc7b3322484", #LG1-SL
-                             "9adf1dbf-58ad-4ba1-bd29-22d25e22c53c", #LG2-SL
-                             "a3ff8527-2582-474f-90b3-0abb7ae0c021", #LG3-SL
-                             "082c2b14-e019-4fb0-9c88-200fa424aaa3", #LG4-SL
-                             "f7362cb4-d4eb-4f83-a140-456141e96b10", #LD2-SM
-                             "01ec2ee7-38db-4a9f-8e1c-5db90301b09e", #LK2-SM
-                             "eee9e685-28b1-4f25-8cb9-67af433f7cc5", #LK1-SM
-                             "1c83b022-c500-4b81-bef5-9b33b36127bb", #LG3-SM
-                             "352f5c2e-2a62-43b6-b8a4-5344c9d30c32", #LG1-SM
-                             "89e106ac-ce69-4d66-9e41-f81e2a014320", #LG4-SM
-                             "420d411a-509a-490d-bc26-9cfd51330ea3", #LG2-SM
-                             "41ffe350-3606-46e6-bc64-0655e6ae959a"] #FB2-SM
-
+                             "f1749739-f4ac-4206-af28-5bc7b3322484",
+                             "9adf1dbf-58ad-4ba1-bd29-22d25e22c53c",
+                             "a3ff8527-2582-474f-90b3-0abb7ae0c021",
+                             "082c2b14-e019-4fb0-9c88-200fa424aaa3",
+                             "f7362cb4-d4eb-4f83-a140-456141e96b10",
+                             "01ec2ee7-38db-4a9f-8e1c-5db90301b09e",
+                             "eee9e685-28b1-4f25-8cb9-67af433f7cc5",
+                             "1c83b022-c500-4b81-bef5-9b33b36127bb",
+                             "352f5c2e-2a62-43b6-b8a4-5344c9d30c32",
+                             "89e106ac-ce69-4d66-9e41-f81e2a014320",
+                             "420d411a-509a-490d-bc26-9cfd51330ea3",
+                             "41ffe350-3606-46e6-bc64-0655e6ae959a"]
 
         emptie_lanes = ["S001.M006.03.01",
                         "S001.M006.03.03",
@@ -209,24 +208,25 @@ class OrganisationEventProcessor(EventProcessor):
     @staticmethod
     def _get_linked_lane(source_address):
         # TODO improve me
-        LINKED_KCC_SOURCE_LANE_ADDRESS = ["S001.M004.01.02", #Linie LD1 im Supermarkt Lieferant
-                                          "S001.M006.02.04", #Linie FB1 im Supermarkt der Montage
-                                          "S001.M006.02.05", #Linie LD1 im Supermarkt Montage
-                                          "S001.M006.01.05", #Linie LD2 im Supermarkt Montage
-                                          "S001.M006.01.04", #Linie FB2 im Supermarkt Montage
-                                          "S001.M006.02.03", #Linie LG1 im Supermarkt Montage
-                                          "S001.M006.01.03", #Linie LG3 im Supermarkt Montage
-                                          "S001.M006.01.02", #Linie LG4 im Supermarkt Montage
-                                          "S001.M006.02.02", #Linie LG2 im Supermarkt Montage
-                                          "S001.M006.02.01", #Linie LK1 im Supermarkt Montage
-                                          "S001.M006.01.01", #Linie LK2 im Supermarkt Montage
+        LINKED_KCC_SOURCE_LANE_ADDRESS = ["S001.M004.01.02",
+                                          "S001.M006.02.04",
+                                          "S001.M006.02.05",
+                                          "S001.M006.01.05",
+                                          "S001.M006.01.04",
+                                          "S001.M006.02.03",
+                                          "S001.M006.01.03",
+                                          "S001.M006.01.02",
+                                          "S001.M006.02.02",
+                                          "S001.M006.02.01",
+                                          "S001.M006.01.01",
                                           "S001.M004.01.01",
                                           "S001.M004.02.01",
-                                          "S001.M004.02.02", #Linie LK1 im Supermarkt Lieferant
-                                          "S001.M004.03.01",  #Linie LG3 im Supermarkt Lieferant
-                                          "S001.M004.03.02", #Linie LG1 im Supermarkt Lieferant
-                                          "S001.M004.04.01", #Linie LG4 im Supermarkt Lieferant
-                                          "S001.M004.04.02", #Linie LG2 im Supermarkt Lieferant
+                                          "S001.M004.02.02",
+                                          "S001.M004.03.01",
+                                          "S001.M004.03.02",
+                                          "S001.M004.04.01",
+                                          "S001.M004.04.02"]
+        LINKED_KCC_TARGET_LANE_ID = ["c63c5b2c-b368-4f15-92ff-812733582ba8",
                                      "aadf95aa-c11e-41a5-adf3-377b540000ab",
                                      "8d20ae37-d762-4d4a-935a-2d07fec2af7e",
                                      "7a5dc1a2-d105-40ec-bcda-5f7c7cb2261b",
