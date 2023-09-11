47d9ae0bd186756a4402eae307d133ea88f46976
otaviomserra <otaviomserra3@gmail.com>
2023-09-11 15:03:00 +0200

    correcting bugs

--- datenverarbeitung/process_requests.py
+++ datenverarbeitung/process_requests.py
@@ -10,28 +10,28 @@ FabrikVerbindung = pd.read_excel("FabrikVerbindung.xlsx", index_col=0)
 
 
 class ProcessRequest:
-    def __init__(self, lane, event):
+    def __init__(self, lane, timestamp):
         # Uses the given origin lane and takes the appropriate target lane from the Excel file
         self.origin_lane = lane
         self.target_lane = FabrikVerbindung.loc[FabrikVerbindung["lane_address"] == lane, "target_lane_address"].iloc[0]
 
         # Events: used to create the process log, put_event is empty for now because the request is still active
-        self.pick_event = event
-        self.put_event = ""
+        self.pick_time = timestamp
+        self.put_time = 0
         self.duration = 0
 
         # Exit code: 1 when request is active, 0 when request is completed, 2 when request is canceled
         self.exit_code = 1
 
-    def resolve(self, event):
-        self.put_event = event
+    def resolve(self, timestamp):
+        self.put_time = timestamp
         # Calculate here the difference in timestamps
-        self.duration = event
+        self.duration = self.put_time - self.pick_time
         # Mark the request as resolved
         self.exit_code = 0
 
     def cancel(self, timestamp):
-        self.duration = timestamp
+        self.duration = timestamp - self.put_time
         # Mark the request as canceled; the system should then remove it from the list without
         # creating a new process log
         self.exit_code = 2
