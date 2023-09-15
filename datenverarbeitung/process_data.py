b65383fa369166de1dca61094258381b84092d04
chicodb <142610159+chicodb@users.noreply.github.com>
2023-09-15 14:38:08 +0200

    kh

---
+++ datenverarbeitung/process_data.py
@@ -0,0 +1,9 @@
+import os
+import pandas as pd
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+werk_folder_path = os.path.join(current_dir, "..", "Werk")
+processes_folder_path = os.path.join(current_dir, "..", "Werk", "Prozesse")
+
+class process_processes
