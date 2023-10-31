630fb47305e043f8aabbe35ad1b755d6d3711c35
otaviomserra <otaviomserra3@gmail.com>
2023-10-31 15:15:21 +0100

    limpando arquivos inúteis

--- datenverarbeitung/run_reinitialization.py
+++ datenverarbeitung/run_reinitialization.py
@@ -4,7 +4,6 @@ import os
 current_dir = os.path.dirname(__file__)
 reinit_file = "initialize_inventory.txt"
 target_file = os.path.join("raw_logs", "reinit.log")
-# target_file = "test_reinit.txt"
 
 reinit = open(reinit_file, "r")
 target = open(target_file, "a")
